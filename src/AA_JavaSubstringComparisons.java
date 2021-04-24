d5c14023d836e79d46749c70c54a1cbe7ae95fd1
Jurijs Cicelimovs <yyyura@gmail.com>
2021-04-24 11:25:22 +0100

    added from Yoga

---
+++ src/AA_JavaSubstringComparisons.java
@@ -0,0 +1,30 @@
+//import java.util.TreeSet;
+
+public class AA_JavaSubstringComparisons {
+
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        java.util.TreeSet<String> ts1 = new java.util.TreeSet<>();
+
+        for (int i = 0; i < s.length() - 3; i++) {
+            ts1.add(s.substring(i, i + 3));
+        }
+
+        System.out.println(ts1.toString());
+
+        smallest = ts1.first();
+        largest = ts1.last();
+
+        return smallest + "\n" + largest;
+    }
+
+
+    public static void main(String[] args) {
+
+        String s = "welcometojava";
+        int k = 3;
+
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
